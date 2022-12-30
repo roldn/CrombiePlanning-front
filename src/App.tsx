403f5b7737d2d6a53fc3a3493159d0deb3a1a52a
Leo <leonardogilli@outlook.com>
2022-12-30 19:50:54 -0300

    room working

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,93 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useEffect, useState } from 'react';
+import './App.css';
+import { io } from 'socket.io-client';
+
+const socket = io('ws://localhost:3000');
+
+type User = {
+  clientId: string;
+  username: string;
+  card: string | null;
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [room, setRoom] = useState<string>();
+  const [username, setUsername] = useState<string>();
+  const [users, setUsers] = useState<User[]>();
+
+  const handleCardSelect = (card: number) => {
+    socket.emit('card', { card, username, room });
+  };
+
+  const createRoom = () => {
+    socket.emit('create_room', username);
+  };
+
+  useEffect(() => {
+    socket.on('new_room', newRoom => {
+      setRoom(newRoom);
+    });
+
+    socket.on('users', users => {
+      setUsers(users);
+    });
+  }, []);
+
+  const joinRoom = () => {
+    socket.emit('join_room', { room, username });
+  };
+
+  const fiboCards = [0, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89];
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='App'>
+      <h1>{room}</h1>
+
+      <button
+        disabled={!username}
+        onClick={createRoom}>
+        Create Room
+      </button>
+
+      <input
+        placeholder='Username'
+        type='text'
+        onChange={e => setUsername(e.target.value)}
+      />
+
+      <input
+        placeholder='Room Number'
+        type='text'
+        onChange={e => setRoom(e.target.value)}
+      />
+      <button
+        disabled={!room || !username}
+        onClick={joinRoom}>
+        Join Room
+      </button>
+
+      <div className='card-container'>
+        {users?.length &&
+          users.map(user => (
+            <div key={user.clientId}>
+              <div className='card received-card'>{user.card}</div>
+              <div className='user'>{user.username}</div>
+            </div>
+          ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+
+      <div className='card-container'>
+        {fiboCards.map(card => (
+          <div
+            onClick={() => handleCardSelect(card)}
+            className='card'
+            key={card}>
+            {card}
+          </div>
+        ))}
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
